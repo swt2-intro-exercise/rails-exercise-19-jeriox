634bdc2b89e1c3d143ca49aae2e1bb08c16fc510
Julian Baumann <julian.baumann@student.hpi.de>
2019-11-05 22:28:21 +0100

    test works on my machine but fails on travis

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Author model", :type => :model do
   it "should create a new author" do
-    author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+    author = Author.new({first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing"})
     expect(author.first_name).to eq("Alan")
     expect(author.last_name).to eq("Turing")
     expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
