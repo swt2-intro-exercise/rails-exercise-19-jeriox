7a2d800124f0c125d447ebc1642b75c088b20a1e
Julian Baumann <julian.baumann@student.hpi.de>
2019-11-05 22:22:43 +0100

    fixed failing test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Author model", type: :model do
+describe "Author model", :type => :model do
   it "should create a new author" do
     author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
     expect(author.first_name).to eq("Alan")
