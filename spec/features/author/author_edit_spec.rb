ebb6d4f1e4eea63d8f627148b2b4b6f0cf638cac
Julian Baumann <julian.baumann@student.hpi.de>
2019-11-06 10:40:51 +0100

    failing test for #10

--- spec/features/author/author_edit_spec.rb
+++ spec/features/author/author_edit_spec.rb
@@ -6,4 +6,12 @@ describe "Author edit page", type: :feature do
     @alan = Author.find_by(first_name: "Alan")
     visit edit_author_path(@alan)
   end
+
+  it "should save the changes", type: :feature do
+    FactoryBot.create :author
+    @alan = Author.find_by(first_name: "Alan")
+    visit edit_author_path(@alan)
+    page.fill_in 'author[first_name]', with: 'Bob'
+    find('input[type="submit"]').click
+  end
 end
