8a124801e99206c6609b6f8e70aa143ae49fd8d7
Julian Baumann <julian.baumann@student.hpi.de>
2019-11-07 17:51:07 +0100

    closes #16

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe "Paper model", :type => :model do
-  @paper = Paper.new({title: "bla", venue: "blubb", year: 1942})
-  expect(@paper.authors).to be_empty
+  it "should have empty author list" do
+    @paper = Paper.new({title: "bla", venue: "blubb", year: 1942})
+    expect(@paper.authors).to be_empty
+  end
 end
