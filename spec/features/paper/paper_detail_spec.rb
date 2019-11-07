2f71ba5936d28b72148cdc0d1d25d3c3201fa223
Julian Baumann <julian.baumann@student.hpi.de>
2019-11-07 18:11:42 +0100

    closes #17

--- spec/features/paper/paper_detail_spec.rb
+++ spec/features/paper/paper_detail_spec.rb
@@ -4,6 +4,7 @@ describe "Paper detail page", :type => :feature do
   it "should display the authors full name" do
     @paper = FactoryBot.create :paper
     visit paper_path(@paper)
+    save_and_open_page
     expect(page).to have_text(@paper.authors.first.name)
   end
 end
